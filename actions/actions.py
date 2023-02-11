ee2629a1650969c80a41db96ec8c255924200c27
nantalira <87638713+nantalira@users.noreply.github.com>
2023-02-11 15:37:34 +0700

    Tambah Fitur hiburan

--- actions/actions.py
+++ actions/actions.py
@@ -1,9 +1,18 @@
 from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
 from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
-# import json
-# import logging
-# import os
-# import random
+import json
+from youtube_search import YoutubeSearch
+from nltk.corpus import stopwords
+import nltk
+from nltk.tokenize import word_tokenize 
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+import random
+import subprocess
+import requests
+import webbrowser
+import time
+import threading
+import pywhatkit
 from rasa_sdk.executor import CollectingDispatcher
 
 from typing import DefaultDict, Text, Callable, Dict, List, Any, Optional
@@ -351,3 +360,30 @@ class ActionAskAgain(Action):
         else:
             dispatcher.utter_message(text=f"Langsung tanyakan saja jika ada pertanyaan tentang {object_type} lagi, atau ingin menanyakan tentang hal lain?")
         return []
+
+
+class ActionOpenYoutube(Action):
+    def name(self):
+        return "action_query_hiburan"
+
+    def run(self, dispatcher, tracker, domain):
+        subprocess.call("TASKKILL /F /IM chrome.exe", shell=True) 
+        text = tracker.latest_message['text']
+        custom_stop_word_list = ["mainkan", "putarkan", "tentang", "video", "lagu", "seputar"]
+        tokens = word_tokenize(text) 
+        sentence_without_stopword = [k for k in tokens if not k in custom_stop_word_list ] 
+        result = sentence_without_stopword
+        sentence = nltk.tokenize.treebank.TreebankWordDetokenizer().detokenize(result)
+        results = YoutubeSearch(sentence, max_results=5).to_json()
+        hasil_akhir = json.loads(results)
+        mylist=[0,1,2,3,4] 
+        n=random.choice(mylist) 
+        penataan = hasil_akhir["videos"][n]["url_suffix"]
+        webbrowser.open("https://www.youtube.com/" + penataan, new=2, autoraise=True)
+        dispatcher.utter_message(text=f"Oke, saya akan putar {sentence} untuk anda")
+        t = threading.Timer(60.0, self.close_browser)
+        t.start()
+        return []
+
+    def close_browser(self):
+        subprocess.call("TASKKILL /F /IM chrome.exe", shell=True)
